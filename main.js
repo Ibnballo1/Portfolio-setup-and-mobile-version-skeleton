9054854b47f9a9c66a10f6bb17935ab522c98411
Ibnballo1 <belloabdullateef035@gmail.com>
2022-03-08 23:34:33 +0100

    Add functions to eventlistener

--- main.js
+++ main.js
@@ -1,7 +1,22 @@
 let menuBar = document.getElementById('menu_bar');
 let times = document.getElementById("close_menu");
 let hamburgerBtn = document.getElementById('menuBarImg');
-let lists = document.getElementById("menu_list"); 
-let isDisplay = true; 
+let lists = document.getElementById("menu_list");
+let isDisplay = true;
 
-menuBar.addEventListener('click', () => {}); 
+menuBar.addEventListener('click', () => {
+  if (isDisplay) {
+    hamburgerBtn.style.display = 'none';
+    times.style.display = 'block';
+    menuBar.classList.add('icon-menu');
+    lists.classList.add('js-nav-list');
+    isDisplay = false;
+    }
+    else {
+    hamburgerBtn.style.display = 'block';
+    times.style.display = 'none';
+    menuBar.classList.replace('icon-menu', 'nav-menu');
+    lists.classList.replace('js-nav-list', 'dv-nav');
+    isDisplay = true;
+  }
+});
