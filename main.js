416eabbfe8206ccfa021eb5e12be314353cdb8e4
Abraha Kahsay <abrahak81@gmail.com>
2022-03-09 04:13:13 +0300

    Modify js file with variables

--- main.js
+++ main.js
@@ -1,3 +1,5 @@
 let menuBar = document.getElementById('menu_bar');
 let times = document.getElementById("close_menu");
 let hamburgerBtn = document.getElementById('menuBarImg');
+let lists = document.getElementById("menu_list"); 
+let isDisplay = true; 
