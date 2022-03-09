17ed6263b4f2800a9fdeb9fb7195aec1f744688b
Ibnballo1 <belloabdullateef035@gmail.com>
2022-03-09 21:28:04 +0100

    Finish mobile menu

--- main.js
+++ main.js
@@ -1,22 +1,34 @@
 let menuBar = document.getElementById('menu_bar');
 let times = document.getElementById("close_menu");
 let hamburgerBtn = document.getElementById('menuBarImg');
+let closeBar = document.getElementById('close_bar');
 let lists = document.getElementById("menu_list");
+let blurEffect = document.querySelectorAll('.toblur');
 let isDisplay = true;
 
 menuBar.addEventListener('click', () => {
   if (isDisplay) {
     hamburgerBtn.style.display = 'none';
     times.style.display = 'block';
-    menuBar.classList.add('icon-menu');
+    closeBar.classList.add('icon-menu');
     lists.classList.add('js-nav-list');
+
+    for (let i= 0; i < blurEffect.length; i++) {
+      blurEffect[i].classList.add('blurring');
+    }
+
     isDisplay = false;
     }
     else {
     hamburgerBtn.style.display = 'block';
     times.style.display = 'none';
-    menuBar.classList.replace('icon-menu', 'nav-menu');
+    closeBar.classList.replace('icon-menu', 'nav-menu');
     lists.classList.replace('js-nav-list', 'dv-nav');
+
+    for (let i= 0; i < blurEffect.length; i++) {
+      blurEffect[i].classList.remove('blurring');
+    }
+
     isDisplay = true;
   }
 });
