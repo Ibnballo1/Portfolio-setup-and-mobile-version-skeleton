8aba4de8f927b62c115b11c3e3e6739a319f8602
Ibnballo1 <belloabdullateef035@gmail.com>
2022-03-12 13:39:23 +0100

    Fix eslint errors

--- main.js
+++ main.js
@@ -1,10 +1,10 @@
-let menuBar = document.getElementById('menu_bar');
-let times = document.getElementById("close_menu");
-let hamburgerBtn = document.getElementById('menuBarImg');
-let closeBar = document.getElementById('close_bar');
-let lists = document.getElementById("menu_list");
-let hiddenScrol = document.getElementById('hideOverflow');
-let blurEffect = document.querySelectorAll('.toblur');
+const menuBar = document.getElementById('menu_bar');
+const times = document.getElementById('close_menu');
+const hamburgerBtn = document.getElementById('menuBarImg');
+const closeBar = document.getElementById('close_bar');
+const lists = document.getElementById('menu_list');
+const hiddenScrol = document.getElementById('hideOverflow');
+const blurEffect = document.querySelectorAll('.toblur');
 let isDisplay = true;
 
 menuBar.addEventListener('click', () => {
@@ -15,20 +15,20 @@ menuBar.addEventListener('click', () => {
     lists.classList.add('js-nav-list');
     hiddenScrol.classList.add('hide-scroll');
 
-    for (let i= 0; i < blurEffect.length; i++) {
+    for (let i = 0; i < blurEffect.length; i += 1) {
       blurEffect[i].classList.add('blurring');
     }
 
     isDisplay = false;
-    }
-    else {
+  }
+  else {
     hamburgerBtn.style.display = 'block';
     times.style.display = 'none';
     closeBar.classList.replace('icon-menu', 'nav-menu');
     lists.classList.replace('js-nav-list', 'dv-nav');
     hiddenScrol.classList.remove('hide-scroll');
 
-    for (let i= 0; i < blurEffect.length; i++) {
+    for (let i = 0; i < blurEffect.length; i += 1) {
       blurEffect[i].classList.remove('blurring');
     }
 
@@ -38,24 +38,21 @@ menuBar.addEventListener('click', () => {
 
 // Validating form section
 
-
 const form = document.getElementById('form');
 const mailValue = document.getElementById('uemail');
 const errorMessage = document.getElementById('errElement');
 
 form.addEventListener('submit', (e) => {
-  let showMessage = [];
-  if(mailValue.value.toLowerCase() !== mailValue.value){
+  const showMessage = [];
+  if (mailValue.value.toLowerCase() !== mailValue.value) {
     errorMessage.style.display = 'inline-block';
     showMessage.push(`
     Error: Email should be in lowercase,
     you entered ${mailValue.value}
     `);
-  } else{
+  } else {
     form.submit();
   }
   e.preventDefault();
   errorMessage.innerText = showMessage;
 });
-
-
